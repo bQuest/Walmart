734e279e3f39dd60eec57aad5f102af86fe98d6a
Quest <b.quest@hotmai.com>
2022-03-07 02:06:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/walmart/Welcome.kt
@@ -0,0 +1,11 @@
+package com.example.walmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
